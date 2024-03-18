6aef71c93c7eb3ae3edd3d5a723dbc9ee285ad25
Frédéric BARRAU <frederic.barrau@zenika.com>
2024-03-18 19:07:17 +0100

    chore - deployable state

--- kubernetes/pong-operator/internal/controller/pong_controller.go
+++ kubernetes/pong-operator/internal/controller/pong_controller.go
@@ -25,6 +25,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	gamesv1alpha1 "github.com/fredericbarrau/pong-made-with-love.git/api/v1alpha1"
+	appsv1 "k8s.io/api/apps/v1"
 )
 
 // PongReconciler reconciles a Pong object
@@ -37,6 +38,9 @@ type PongReconciler struct {
 //+kubebuilder:rbac:groups=games.fredericbarrau.bzh,resources=pongs/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=games.fredericbarrau.bzh,resources=pongs/finalizers,verbs=update
 
+//+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;update;patch;delete
+
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
 // TODO(user): Modify the Reconcile function to compare the state specified by
@@ -47,9 +51,10 @@ type PongReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.16.3/pkg/reconcile
 func (r *PongReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	log := log.FromContext(ctx)
 
 	// TODO(user): your logic here
+	log.Info("Reconciling Pong")
 
 	return ctrl.Result{}, nil
 }
@@ -58,5 +63,6 @@ func (r *PongReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.
 func (r *PongReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&gamesv1alpha1.Pong{}).
+		Owns(&appsv1.Deployment{}). // Create watches for the Deployment which has its controller owned reference
 		Complete(r)
 }
